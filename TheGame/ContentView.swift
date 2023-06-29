6978723993ba901bed25046ccbffb54edeaec3a6
Marie Top Mygind <marietopmygind@gmail.com>
2023-06-29 20:35:02 +0200

    Added bootstrap xcode project

---
+++ TheGame/ContentView.swift
@@ -0,0 +1,19 @@
+// Henrik Top Mygind, 29/06/2023
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheGame/TheGameApp.swift
@@ -0,0 +1,12 @@
+// Henrik Top Mygind, 29/06/2023
+
+import SwiftUI
+
+@main
+struct TheGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
